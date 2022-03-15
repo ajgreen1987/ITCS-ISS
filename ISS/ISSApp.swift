db026eadbbbb73254213d43a5872c017abcaac93
AJ Green <me@ajgreen.codes>
2022-03-14 21:49:06 -0400

    Initial Commit

---
+++ ISS/ISSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISSApp.swift
+//  ISS
+//
+//  Created by AJ Green on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ISSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
